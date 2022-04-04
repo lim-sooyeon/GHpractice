05ea70343771a03b8b83c4c82a272311eda38a9e
lim-sooyeon <sooyeon7434@naver.com>
2022-04-04 15:53:12 +0900

    first github test

---
+++ java tutorials/src/org/opentutorial/javatutorial/eclipse/Hellowolrd.java
@@ -0,0 +1,10 @@
+package org.opentutorial.javatutorial.eclipse;
+
+public class Hellowolrd {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		// first github test
+	}
+
+}
